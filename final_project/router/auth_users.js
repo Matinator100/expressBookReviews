f8501a7fe68e0f460b26de800d7cfbbd34f9b2f4
Matthew Williams <williams.matthew40@yahoo.com>
2023-03-10 17:44:37 -0500

    Adjust responses to match test
    Signed-off-by: Matthew Williams <williams.matthew40@yahoo.com>

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,7 +6,8 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-  if(users.indexOf(username) >= 0){
+  let user = users.filter(user => user.username === username)
+  if(user.length){
     return false;
   }
   return true;
@@ -25,37 +26,36 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  let user = req.body.user;
-  if (!user) {
-      return res.status(404).json({message: "Body Empty"});
+  if (!req.body.username || !req.body.password) {
+      return res.status(404).json({message: "Please enter valid credentials"});
   }
-  else if(!authenticatedUser(user.username, user.password)){
-      return res.status(404).json({message: "No user with specified credentials found"});
+  else if(!authenticatedUser(req.body.username, req.body.password)){
+      return res.status(404).json({message: "No Customer with specified credentials found"});
   }
   let accessToken = jwt.sign({
-      data: user
+      data: {username: req.body.username, password: req.body.password}
     }, 'access', { expiresIn: 60 * 60 });
 
     req.session.authorization = {
       accessToken
   }
-  return res.status(200).send("User successfully logged in");
+  return res.status(200).json({message:"Customer successfully logged in"});
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  let review = req.body.review;
+  let review = req.query.review;
   if(review){
     books[req.params.isbn].reviews[req.user.data.username] = review
-    return res.status(200).send(books[req.params.isbn])
+    return res.status(200).json({message: `The review for book with ISBN ${req.params.isbn} has been added/updated`})
   }
   return res.status(404).json({message: "No review provided"});
 });
 
-// Add a book review
+// Delete a book review
 regd_users.delete("/auth/review/:isbn", (req, res) => {
   delete books[req.params.isbn].reviews[req.user.data.username]
-  return res.status(200).send({message : "Review Deleted"})
+  return res.status(200).json({message : `Reviews for the book with ISBN ${req.params.isbn} posted by ${req.user.data.username} has been deleted`})
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,25 +12,25 @@ public_users.post("/register", (req,res) => {
   if (username && password) {
     if (isValid(username)) { 
       users.push({"username":username,"password":password});
-      return res.status(200).json({message: "User successfully registred. Now you can login"});
+      return res.status(200).json({message: "Customer successfully registered. Now you can login"});
     } else {
-      return res.status(404).json({message: "User already exists!"});    
+      return res.status(404).json({message: "Customer already exists!"});    
     }
   } 
-  return res.status(404).json({message: "Unable to register user."});
+  return res.status(404).json({message: "Unable to register ustomer."});
 });
 
 // Get the book list available in the shop
 public_users.get('/', async function (req, res) {
   //Write your code here
-  return res.send(JSON.stringify(books, null, 2));
+  return res.status(200).json({books : books});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', async function (req, res) {
   let book = books[req.params.isbn]
   if(book){
-    return res.send(book)
+    return res.status(200).json(book)
   }
   return res.json({message: "No book with that ISBN found"});
  });
@@ -38,13 +38,13 @@ public_users.get('/isbn/:isbn', async function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author', async function (req, res) {
   let authorBooks = Object.values(books).filter(book => book.author === req.params.author)
-  return res.send(JSON.stringify(authorBooks, null, 2));
+  return res.status(200).json({booksbyauthor: authorBooks});
 });
 
 // Get all books based on title
 public_users.get('/title/:title', async function (req, res) {
   let titleBooks = Object.values(books).filter(book => book.title === req.params.title)
-  return res.send(JSON.stringify(titleBooks, null, 2));
+  return res.status(200).json({booksbytitle: titleBooks});
 });
 
 //  Get book review
